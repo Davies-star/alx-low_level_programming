53cd2762665eb539ea1fac4e30723106aa91f5b8
Davies-star <chiazodave@gmail.com>
2023-02-21 18:13:48 -0800

    2-alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+
+	char alp;
+	int i = 0;
+
+	while (i <= 0)
+	{
+		for (alp = 'a'; alp <= 'z'; alp++)
+		{
+			_putchar(alp);
+
+			_putchar('\n');
+			i++;
+		}
+	}
+}
