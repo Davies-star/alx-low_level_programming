c3d7c7509a2f17b87320f1f4f9ecfa3d40d9883a
Davies-star <chiazodave@gmail.com>
2023-02-23 18:56:37 -0800

    bug

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+*main - Prints if the number is greter less or exactly one
+*
+*Return: Always (Success)
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%i is positive\n", n);
+	else if (n < 0)
+		printf("%i is negative\n", n);
+	else
+		printf("%i is zero\n", n);
+	return (0);
+}
